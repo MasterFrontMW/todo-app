5840f820206511550a2955f8226d3fef45460b39
unknown <maciopul@gmail.com>
2022-09-28 11:42:25 +0200

    added button to edit task

--- src/ToDoItem/ToDoItem.js
+++ src/ToDoItem/ToDoItem.js
@@ -9,20 +9,39 @@ export const ToDoItem = (message) => {
   checkbox.type = "checkbox";
   checkbox.classList.add("to-do-element-checkbox");
 
+  const checkToDoElement = () => {
+    if (toDoMessage.style.textDecoration == "line-through")
+      toDoMessage.style.textDecoration = "none";
+    else toDoMessage.style.textDecoration = "line-through";
+  }
+
   // TODO: show added message from AddToDoField inside task
   const toDoMessage = document.createElement("p");
-  toDoMessage.innerText = "test message";
+  toDoMessage.innerText = message;
+  
+
+  //TODO: add button to edit TodoElement
+  const editButton = document.createElement("div");
+  editButton.classList.add("edit-button");
+
+  const editToDoButton = () => {
+    toDoMessage.contentEditable = true;
+    toDoMessage.focus()
+  }
 
   const closeButton = document.createElement("div");
   closeButton.classList.add("close-button");
 
   // TODO: remove todo element from DOM on close click
-  closeButton.addEventListener("click", () =>
-    toDoItemElement.remove()
-  );
+  closeButton.addEventListener("click", () => toDoItemElement.remove());
+
+  editButton.addEventListener("click", editToDoButton)
+
+  checkbox.addEventListener("click", checkToDoElement);
 
   toDoItemElement.appendChild(checkbox);
   toDoItemElement.appendChild(toDoMessage);
+  toDoItemElement.appendChild(editButton);
   toDoItemElement.appendChild(closeButton);
 
   return toDoItemElement;
