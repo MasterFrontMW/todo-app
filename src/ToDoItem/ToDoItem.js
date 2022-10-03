23c28da768a24c60677647ded74a7b5562e0c71d
unknown <maciopul@gmail.com>
2022-10-03 12:38:32 +0200

    feat(TDL-25,29: deleted empty lines, greyed out task when checked, editing not avaliable when checked, checkbox has hover: pointer)

--- src/ToDoItem/ToDoItem.js
+++ src/ToDoItem/ToDoItem.js
@@ -9,37 +9,40 @@ export const ToDoItem = (message) => {
   checkbox.type = "checkbox";
   checkbox.classList.add("to-do-element-checkbox");
 
-  const checkToDoElement = () => {
-    if (toDoMessage.style.textDecoration == "line-through")
-      toDoMessage.style.textDecoration = "none";
-    else toDoMessage.style.textDecoration = "line-through";
-  }
+  const checkToDoElement = (e) => {
+    e.target.checked
+      ? (toDoMessage.style.textDecoration = "line-through")
+        (toDoMessage.style.opacity = 0.4)
+      : (toDoMessage.style.textDecoration = "none");
+        (toDoMessage.style.opacity = 1);
+    
+  };
 
   // TODO: show added message from AddToDoField inside task
   const toDoMessage = document.createElement("p");
-  toDoMessage.classList.add("task-message")
+  toDoMessage.classList.add("task-message");
   toDoMessage.innerText = message;
-  
 
   //TODO: add button to edit TodoElement
   const editButton = document.createElement("div");
   editButton.classList.add("edit-button");
 
+  // const editToDoButton = (e) => {toDoMessage.contentEditable = e.target.checked ? false : true}
+
   const editToDoButton = () => {
-    toDoMessage.contentEditable = true;
-    toDoMessage.focus()
-  }
+    if (checkbox.checked == false) {
+      toDoMessage.contentEditable = true;
+      toDoMessage.focus();
+    } else toDoMessage.contentEditable = false;
+  };
 
   const closeButton = document.createElement("div");
   closeButton.classList.add("close-button");
 
   // TODO: remove todo element from DOM on close click
   closeButton.addEventListener("click", () => toDoItemElement.remove());
-
-  editButton.addEventListener("click", editToDoButton)
-
+  editButton.addEventListener("click", editToDoButton);
   checkbox.addEventListener("click", checkToDoElement);
-
   toDoItemElement.appendChild(checkbox);
   toDoItemElement.appendChild(toDoMessage);
   toDoItemElement.appendChild(editButton);
