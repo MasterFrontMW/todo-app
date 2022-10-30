c5e921b9e94a461667192a8496dc2c062192d5a3
macopul <105385617+macopul@users.noreply.github.com>
2022-10-30 20:39:45 +0100

    feat(TDL-28,25): added style to appear new task in fancy way, change icon to edit to icon from google icons (#17)

--- src/ToDoItem/ToDoItem.js
+++ src/ToDoItem/ToDoItem.js
@@ -9,20 +9,42 @@ export const ToDoItem = (message) => {
   checkbox.type = "checkbox";
   checkbox.classList.add("to-do-element-checkbox");
 
+  const checkToDoElement = (e) => {
+    e.target.checked
+      ? (toDoMessage.style.textDecoration = "line-through")(
+          (toDoMessage.style.opacity = 0.4)
+        )
+      : (toDoMessage.style.textDecoration = "none");
+    toDoMessage.style.opacity = 1;
+  };
+
   // TODO: show added message from AddToDoField inside task
   const toDoMessage = document.createElement("p");
-  toDoMessage.innerText = "test message";
+  toDoMessage.classList.add("task-message");
+  toDoMessage.innerText = message;
+
+  //TODO: add button to edit TodoElement
+  const editButton = document.createElement("div");
+  editButton.classList.add("edit-button");
+
+  const editToDoButton = () => {
+    if (checkbox.checked) {
+      return;
+    }
+    toDoMessage.contentEditable = true;
+    toDoMessage.focus();
+  };
 
   const closeButton = document.createElement("div");
   closeButton.classList.add("close-button");
 
   // TODO: remove todo element from DOM on close click
-  closeButton.addEventListener("click", () =>
-    toDoItemElement.remove()
-  );
-
+  closeButton.addEventListener("click", () => toDoItemElement.remove());
+  editButton.addEventListener("click", editToDoButton);
+  checkbox.addEventListener("click", checkToDoElement);
   toDoItemElement.appendChild(checkbox);
   toDoItemElement.appendChild(toDoMessage);
+  toDoItemElement.appendChild(editButton);
   toDoItemElement.appendChild(closeButton);
 
   return toDoItemElement;
