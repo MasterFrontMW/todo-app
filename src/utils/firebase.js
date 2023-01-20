ea0753fe2b99e4e5efed08008054ea1ba8cf8169
Wojtek Opulski <wojtekopulski@gmail.com>
2023-01-21 00:11:22 +0100

    feat(TDL-42): Add Firebase hosting config and github actions - preview pages for PRs (#32)

---
+++ src/utils/firebase.js
@@ -0,0 +1,23 @@
+/* eslint-disable */
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+import { getAnalytics } from 'firebase/analytics';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: 'AIzaSyAP_SuzcQC0fAtI_V9Dmj28lFh4N7s0v0U',
+  authDomain: 'to-do-app-master-front.firebaseapp.com',
+  projectId: 'to-do-app-master-front',
+  storageBucket: 'to-do-app-master-front.appspot.com',
+  messagingSenderId: '601129874808',
+  appId: '1:601129874808:web:e4196dc85c4696f779848d',
+  measurementId: 'G-52B0K87N3Q',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
