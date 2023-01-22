773f555b15294e34cc69e365bb8d7f7b6f3be705
Wojtek Opulski <wojtekopulski@gmail.com>
2023-01-22 15:03:12 +0100

    updated with missing accordion initialization

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 // HERE WE WILL WRITE SOME ENDING JS SCRIPTS TO MANIPULATE PAGE
-import './src/utils/firebase';
+
 import { Title } from './src/Title/Title';
+
 import { AddToDoField, initializeAddToDoField } from './src/AddToDoItem/AddToDoField';
 import { getTasksDataFromLocalStorage } from './src/helpers/storage';
 import { ToDoItem } from './src/ToDoItem/ToDoItem';
+import { Accordion, initializeAccordion } from './src/Accordion/Accordion';
 
 // selectors
 const pageTitle = document.querySelector('#title');
+const accordion = document.querySelector('#accordion');
 const addToDoSection = document.querySelector('#todo-section');
 
 // load data from local storage and render tasks
@@ -21,7 +24,8 @@ const initializeApp = () => {
   renderTasks();
   pageTitle.innerHTML = Title();
   addToDoSection.append(AddToDoField());
-
+  accordion.innerHTML = Accordion();
+  initializeAccordion(accordion);
   initializeAddToDoField(addToDoSection);
 };
 
